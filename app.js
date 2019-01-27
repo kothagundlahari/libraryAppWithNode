0c7d3141ddee55e071662a10a496e8b1a5dd9627
hari <hari.k.krishna4@gmail.com>
2019-01-27 10:17:58 -0700

    added the debug

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('app');
+
+var app = express();
+var port = 3000;
+
+app.get('/', function(req, res){
+   res.send('Hello from the library app');
+});
+
+app.listen(port, function(){
+  debug('Listening on port: '+ chalk.green(port));
+} );
