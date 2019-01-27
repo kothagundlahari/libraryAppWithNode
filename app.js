a573fc9dd8dc1ea6b38546b710ad850070e56627
hari <hari.k.krishna4@gmail.com>
2019-01-27 10:27:45 -0700

    added the morgan module for getting the networking data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var chalk = require('chalk');
 var debug = require('debug')('app');
+var morgan = require('morgan');
 
 var app = express();
 var port = 3000;
@@ -8,6 +9,8 @@ var port = 3000;
 app.get('/', function(req, res){
    res.send('Hello from the library app');
 });
+
+app.use(morgan('tiny'));
 
 app.listen(port, function(){
   debug('Listening on port: '+ chalk.green(port));
