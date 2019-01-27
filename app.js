f2634084b8b1ede099e5b58158919bd47badb46c
hari <hari.k.krishna4@gmail.com>
2019-01-27 10:39:20 -0700

    add the path serve a html page

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ var express = require('express');
 var chalk = require('chalk');
 var debug = require('debug')('app');
 var morgan = require('morgan');
+var path = require('path');
 
 var app = express();
 var port = 3000;
 
-app.get('/', function(req, res){
-   res.send('Hello from the library app');
-});
 
 app.use(morgan('tiny'));
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname,'/views/index.html'))
+});
 
 app.listen(port, function(){
   debug('Listening on port: '+ chalk.green(port));
