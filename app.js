055cb7068bfddabf25e5c7f4764518774861132f
hari <hari.k.krishna4@gmail.com>
2019-02-04 03:03:57 -0700

    used node moudles to serve static files

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 var express = require('express');
-var chalk = require('chalk');
 var debug = require('debug')('app');
+var chalk = require('chalk');
 var morgan = require('morgan');
 var path = require('path');
 
+
+
 var app = express();
-var port = 3000;
 
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
+
 
 app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname,'/views/index.html'))
+   res.sendFile(path.join(__dirname, 'views/index.html'))
 });
 
-app.listen(port, function(){
-  debug('Listening on port: '+ chalk.green(port));
+app.listen(3000, function () {
+    debug(`Listening on port ${chalk.green('3000')}`);
 } );
